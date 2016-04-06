d227223c43856ac075f2e0043f54accff7e8438e
OleshkoK <iv1235@mail.ru>
2016-04-06 16:07:35 +0200

    DataFixture.2

--- src/Web/SecurityBundle/Entity/Admin.php
+++ src/Web/SecurityBundle/Entity/Admin.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Admin
  *
- * @ORM\Table(name="admin")
+ * @ORM\Table(name="admin",indexes={@ORM\Index(name="name_email_idx", columns={"name", "email"})})
  * @ORM\Entity(repositoryClass="Web\SecurityBundle\Repository\AdminRepository")
  */
 class Admin extends BaseUser
