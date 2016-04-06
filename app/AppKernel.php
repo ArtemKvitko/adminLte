26ae0e8506ee5c066a60cfc176e064be7f681cba
Roman_Ivaniv <ivaniwroman@gmail.com>
2016-04-06 11:33:27 +0300

    add Fixtues bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Web\LoydAdminBundle\LoydAdminBundle(),
             new Web\SecurityBundle\WebSecurityBundle(),
+
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -26,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new Symfony\Bundle\AsseticBundle\AsseticBundle();
+            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
         }
 
         return $bundles;
