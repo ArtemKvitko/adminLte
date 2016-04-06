8c434abd3e702843f8e4eb05f115b28634346e60
vasyl-komarychyn-8 <vasyl.komarychyn@loyd.international>
2016-04-06 11:51:21 +0300

    Login page in another bundle, configs, login routes...

--- src/Web/SecurityBundle/Controller/LoginController.php
+++ src/Web/SecurityBundle/Controller/LoginController.php
@@ -6,8 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LoginController extends Controller
 {
-    public function indexAction()
+    public function loginAction()
     {
-        return $this->render('WebSecurityBundle:Default:index.html.twig');
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('WebSecurityBundle:Login:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
+
+    /**
+     * Never used due to Symfony's Security component
+     */
+    public function loginCheckAction(){}
+
+    /**
+     * Never used due to Symfony's Security component
+     */
+    public function logoutAction(){}
 }
