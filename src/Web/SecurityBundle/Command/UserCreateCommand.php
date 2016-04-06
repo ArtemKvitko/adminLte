d84ecdfa4f4cd7e94f29380a3df0cbc15ea9cdbb
vasyl-komarychyn-8 <vasyl.komarychyn@loyd.international>
2016-04-06 11:40:20 +0300

    CLI for creating users. BaseUser etc...

---
+++ src/Web/SecurityBundle/Command/UserCreateCommand.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Web\SecurityBundle\Command;
+
+use Web\SecurityBundle\WebSecurityBundle;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Web\SecurityBundle\Entity;
+
+class UserCreateCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('loyd:user:create')
+            ->setDescription('Creates user')
+            ->addOption('username', null, InputOption::VALUE_OPTIONAL, 'User\'s login')
+            ->addOption('email', null, InputOption::VALUE_OPTIONAL, 'User\'s email')
+            ->addOption('name', null, InputOption::VALUE_OPTIONAL, 'User\'s name')
+            ->addOption('surname', null, InputOption::VALUE_OPTIONAL, 'User\'s surname')
+            ->addOption('password', null, InputOption::VALUE_OPTIONAL, 'User\'s password')
+            ->addOption('role', null, InputOption::VALUE_OPTIONAL, 'User\'s role [ROLE_ADMIN]')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $login = trim($input->getOption('username'));
+        if(empty($login)){
+            $login = $io->ask('New user\'s login', '', function ($login) {
+                if (empty($login)) {
+                    throw new \RuntimeException('Login cannot be empty.');
+                }
+
+                return $login;
+            });
+        }
+
+        $email = trim($input->getOption('email'));
+        if(empty($email)){
+            $email = $io->ask('New user\'s email', '', function ($email) {
+                if (empty($email)) {
+                    throw new \RuntimeException('Email cannot be empty.');
+                }
+
+                return $email;
+            });
+        }
+
+        $password = trim($input->getOption('password'));
+        if(empty($password)) {
+            $password = $io->askHidden('New user\'s password:', function ($password) {
+                if (empty($password)) {
+                    throw new \RuntimeException('Password cannot be empty.');
+                }
+                if (strlen($password) < 6) {
+                    throw new \RuntimeException('Password has to be longer than 5 characters.');
+                }
+
+                return $password;
+            });
+        }
+
+        $name = trim($input->getOption('name'));
+        if(empty($name)){
+            $name = $io->ask('New user\'s name');
+        }
+
+        $role = trim($input->getOption('role'));
+        if(empty($role)){
+            $role = $io->ask('User\'s role', '', function ($role) {
+                if (empty($role)) {
+                    return $role = 'ROLE_ADMIN';
+                }else{
+                    return $role;
+                }
+
+            });
+        }
+
+        $surname = trim($input->getOption('surname'));
+        if(empty($surname)){
+            $surname = $io->ask('New user\'s surname');
+        }
+
+        $user = new Entity\User();
+        $user->setIsActive(true);
+        $user->setUsername($login);
+        $user->setEmail($email);
+        $user->setName($name);
+        $user->setRole($role);
+        $user->setSurname($surname);
+
+        $encoder = $this->getContainer()->get('security.encoder_factory')->getEncoder($user);
+        $password = $encoder->encodePassword($password, $user->getSalt());
+        $user->setPassword($password);
+
+        $manager = $this->getContainer()->get('doctrine')->getManager();
+        $manager->persist($user);
+        $manager->flush();
+
+        $output->writeln('User has been created.');
+    }
+}
---
+++ src/Web/SecurityBundle/Entity/BaseUser.php
@@ -0,0 +1,296 @@
+<?php
+
+namespace Web\SecurityBundle\Entity;
+use Doctrine\Common\Collections;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * BaseDcsPilots
+ *
+ * @ORM\MappedSuperclass
+ */
+abstract class BaseUser implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=32, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=60, unique=true)
+     */
+    private $email;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255, unique=true)
+     */
+    private $surname;
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="isActive", type="boolean", nullable=true)
+     */
+    private $isActive;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="roles", type="string", nullable=true, length=255,
+     *     columnDefinition="ENUM('ROLE_USER', 'ROLE_TEAM', 'ROLE_OPERATOR', 'ROLE_ADMIN', 'ROLE_ROOT')",
+     *     options={"default" = "ROLE_USER"})
+     */
+    private $roles;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    /**
+     * Set roles
+     *
+     * @param string $roles
+     * @return User
+     */
+    public function setRole($roles)
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    /**
+     * Get roles
+     *
+     * @return string
+     */
+    public function getRoles()
+    {
+        return array($this->roles);
+    }
+    public function setRoles($roles){
+        $this->roles = $roles;
+    }
+    public function getRole()
+    {
+        return $this->roles;
+    }
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return User
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    public function eraseCredentials() {
+
+    }
+
+    public function getSalt() {
+
+    }
+
+    public function serialize() {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    public function unserialize($serialized) {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return BaseUser
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     * @return BaseUser
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string 
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
