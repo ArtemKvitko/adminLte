9417a9c3a91d1c633ba301110924f65e9ec0e049
OleshkoK <iv1235@mail.ru>
2016-04-07 13:56:43 +0200

    Redirect

--- src/Web/SecurityBundle/Controller/LoginController.php
+++ src/Web/SecurityBundle/Controller/LoginController.php
@@ -3,28 +3,32 @@
 namespace Web\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
         $authenticationUtils = $this->get('security.authentication_utils');
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
-
+        
+        
         return $this->render('WebSecurityBundle:Login:login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
         ));
+        
     }
 
     /**
      * Never used due to Symfony's Security component
      */
-    public function loginCheckAction(){}
+    public function loginCheckAction(){
+        
+    }
 
-    /**
-     * Never used due to Symfony's Security component
-     */
+    
+    
     public function logoutAction(){}
 }
