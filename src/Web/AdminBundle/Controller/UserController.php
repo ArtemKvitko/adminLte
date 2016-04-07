3f6691fb5fec1c832e2c9a6fdd8f468dbf9d1bb7
Roman_Ivaniv <ivaniwroman@gmail.com>
2016-04-07 10:48:59 +0300

    Second user crud

---
+++ src/Web/AdminBundle/Controller/UserController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Web\AdminBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Web\SecurityBundle\Entity\User;
+use Web\AdminBundle\Form\UserType;
+
+/**
+ * User controller.
+ *
+ */
+class UserController extends Controller
+{
+    /**
+     * Lists all ser entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $user = $em->getRepository('WebSecurityBundle:User')->findAll();
+
+        return $this->render('WebAdminBundle:User:index.html.twig', array(
+            'Users' => $user,
+        ));
+    }
+
+    /**
+     * Creates a new User entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm('Web\AdminBundle\Form\UserType', $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_users_show', array('id' => $user->getId()));
+        }
+
+        return $this->render('WebAdminBundle:User:new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a User entity.
+     *
+     */
+    public function showAction(User $user)
+    {
+
+
+        return $this->render('WebAdminBundle:User:show.html.twig', array(
+            'User' => $user,
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing User entity.
+     *
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $editForm = $this->createForm('Web\AdminBundle\Form\UserType', $user);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_users_edit', array('id' => $user->getId()));
+        }
+
+        return $this->render('WebAdminBundle:User:edit.html.twig', array(
+            'User' => $user,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a User entity.
+     *
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        return $this->redirectToRoute('admin_users_index');
+    }
+}
+
