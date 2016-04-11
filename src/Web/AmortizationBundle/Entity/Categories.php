ae28d3a54efe0b9eff057165f2230207709eb280
ArtemKvitko <artem.kvitko@loyd.international>
2016-04-11 10:37:20 +0300

    Assets work with categories

--- src/Web/AmortizationBundle/Entity/Categories.php
+++ src/Web/AmortizationBundle/Entity/Categories.php
@@ -121,4 +121,7 @@ class Categories
     {
         return $this->usefulTime;
     }
+    public function __toString() {
+        return $this->getTitle();
+    }
 }
--- src/Web/AssetsBundle/Form/AssetsType.php
+++ src/Web/AssetsBundle/Form/AssetsType.php
@@ -8,42 +8,46 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class AssetsType extends AbstractType
-{
+class AssetsType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('title',TextType::class, array('label' => 'Asset name:',
+                ->add('title', TextType::class, array('label' => 'Asset name:',
                     'attr' => array('class' => 'form-control')
                 ))
-            ->add('inventaryNumber',TextType::class, array('label' => 'Inventary number:',
+                ->add('category', EntityType::class, array('required' => true,
+                    'placeholder' => "Choose category:",
+                    'class' => 'WebAmortizationBundle:Categories', 'attr' => array(
+                        'class' => 'form-control')
+                ))
+                ->add('inventaryNumber', TextType::class, array('label' => 'Inventary number:',
                     'attr' => array('class' => 'form-control')
                 ))
-            ->add('initialcost',TextType::class, array('label' => 'Initial cost:',
+                ->add('initialcost', TextType::class, array('label' => 'Initial cost:',
                     'attr' => array('class' => 'form-control')
                 ))
-            ->add('replacementcost',TextType::class, array('label' => 'Replacement cost:',
+                ->add('replacementcost', TextType::class, array('label' => 'Replacement cost:',
                     'attr' => array('class' => 'form-control')
                 ))
-            ->add('city_id', EntityType::class, array('required'=>false,
-                'placeholder' => "Choose asset city:", 
-                'class' => 'WebCitiesBundle:Cities', 'attr' => array(
+                ->add('city', EntityType::class, array('required' => false,
+                    'placeholder' => "Choose asset city:",
+                    'class' => 'WebCitiesBundle:Cities', 'attr' => array(
                         'class' => 'form-control')
                 ))
         ;
     }
-    
+
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
+    public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Web\AssetsBundle\Entity\Assets'
         ));
     }
+
 }
